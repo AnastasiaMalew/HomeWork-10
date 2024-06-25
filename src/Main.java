82545376271c9a06fbf1cbb079bf1e7faf797bbe
Nastia <anastasiamalew@gmail.com>
2024-06-25 23:15:11 +0300

    HW10

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,20 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задание 1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println(fullName);
 
+        System.out.println("Задание 2");
+        String small = fullName;
+        System.out.println(small.toUpperCase());
+
+        System.out.println("Задание 3");
+        String fullName2 = "Иванов Семён Семёнович";
+        String amend = fullName2.replace('ё', 'е');
+        System.out.println("ФИО сотрудника: " + amend);
     }
 }
