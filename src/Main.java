fa5fb03cf78d88a41c9c839b167b6c84ea0887b7
Nastia <anastasiamalew@gmail.com>
2024-06-26 21:46:33 +0300

    HW10

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задание 1");
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
-        System.out.println(fullName);
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф.И.О. сотрудника — " + fullName);
 
         System.out.println("Задание 2");
-        String small = fullName;
-        System.out.println(small.toUpperCase());
+        String fullNameUpperCase = fullName;
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета — " + fullNameUpperCase.toUpperCase());
 
         System.out.println("Задание 3");
         String fullName2 = "Иванов Семён Семёнович";
         String amend = fullName2.replace('ё', 'е');
-        System.out.println("ФИО сотрудника: " + amend);
+        System.out.println("Данные Ф.И.О. сотрудника — " + amend);
     }
 }
